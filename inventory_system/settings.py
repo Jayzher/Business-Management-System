4542e00a7e80e9e8aff813e84cb66a7664cedc72
jayzher <jayzherjuaniza@gmail.com>
2026-03-30 17:51:05 +0800

    Changes 3/30/2026

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -83,6 +83,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'theme.context_processors.sidebar_menu',
+                'theme.context_processors.user_role_flags',
             ],
         },
     },
